4284d2c158da8d01bbf9e5cd4fe68d9266d881ee
sigure0314 <70961895+sigure0314@users.noreply.github.com>
2025-12-17 18:02:06 +0000

    新增相關程式碼

---
+++ MyCMS.API/Services/TokenService.cs
@@ -0,0 +1,32 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using MyCMS.API.Models;
+
+namespace MyCMS.API.Services;
+
+public class TokenService {
+    private readonly IConfiguration _config;
+    public TokenService(IConfiguration config) => _config = config;
+
+    public string CreateToken(User user) {
+        var claims = new List<Claim> {
+            new(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+            new(JwtRegisteredClaimNames.UniqueName, user.Username),
+            new(ClaimTypes.Role, user.Role.Name)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var token = new JwtSecurityToken(
+            issuer: _config["Jwt:Issuer"],
+            audience: _config["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddDays(1),
+            signingCredentials: creds
+        );
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
