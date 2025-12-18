9ef815df4858b49a5ef628223ccd31288b9c411c
sigure0314 <70961895+sigure0314@users.noreply.github.com>
2025-12-18 03:37:26 +0000

    add auth ts

---
+++ my-cms-client/src/types/auth.ts
@@ -0,0 +1,3 @@
+export interface LoginRequest { username: string; password: string; }
+export interface RegisterRequest { username: string; password: string; email: string; }
+export interface AuthResponse { token: string; username: string; role: string; }
