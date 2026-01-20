40391befa35db8d51529da0121c00c33feacce21
sigure0314 <70961895+sigure0314@users.noreply.github.com>
2026-01-20 17:45:04 +0000

    merge kitchen table

--- MyCMS.API/Migrations/KitchenDbContext/20260120174345_InitialKitchen.Designer.cs
+++ MyCMS.API/Migrations/KitchenDbContext/20260120174345_InitialKitchen.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyCMS.API.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MyCMS.API.Migrations.Kitchen
 {
     [DbContext(typeof(KitchenDbContext))]
-    partial class KitchenDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260120174345_InitialKitchen")]
+    partial class InitialKitchen
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
