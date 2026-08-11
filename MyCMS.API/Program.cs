b6a72ef87616ba0834c492c1c4d47dbc61f3cda1
sigure0314 <70961895+sigure0314@users.noreply.github.com>
2026-08-11 17:26:26 +0800

    Repair property management schema on existing databases

--- MyCMS.API/Program.cs
+++ MyCMS.API/Program.cs
@@ -155,6 +155,11 @@ app.UseSerilogRequestLogging();
 if (app.Environment.IsDevelopment()) { app.UseSwagger(); app.UseSwaggerUI(); }
 
 using (var scope = app.Services.CreateScope()) {
+    var appDb = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    if (appDb.Database.IsRelational()) {
+        appDb.Database.Migrate();
+    }
+
     var kitchenDb = scope.ServiceProvider.GetRequiredService<KitchenDbContext>();
     if (kitchenDb.Database.IsRelational()) {
         kitchenDb.Database.Migrate();
